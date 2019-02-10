fc3855e5453db6f49d2df97d89d6b409e363a140
André Vincent <andrevncnt7@gmail.com>
2019-02-10 15:33:04 -0500

    helper function for adding to new list

--- fpl.scm
+++ fpl.scm
@@ -1,10 +1,15 @@
 (define (reverse-general L)
+  (define reverseList ()) ;;initializes the reverseList - empty
   (cond
-   ((null? 'L)
-    '#t
+   ((null? 'L) ;; if L is an empty list - 
+     #t        ;; function is done, return first list
       )
-   (else #f)
+   (else '#f) ;; if L is not an empty list - 
    )
   )
 
-(display (reverse-general '()))
+(define (AddToNewList origList newList)
+  (cons 
+   (car originList)
+   newlist)
+  )
