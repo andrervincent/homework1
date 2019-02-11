be21aba48a2522e72554f630cecdfdeeeddcf563
André Vincent <andrevncnt7@gmail.com>
2019-02-10 19:14:49 -0500

    Adds proposed solution for the 2nd problem

--- fpl.scm
+++ fpl.scm
@@ -1,3 +1,4 @@
+;; Reverse List problem
 (define (reverse-general L)
   (cond ((null? L) '()) ;; if the param is null return the param
         ((list? L)      ;; if the param is a list, 
@@ -10,3 +11,16 @@
   )
 
 (display (deep-reverse '(1 2 (7 8))))
+
+;; Sum of list problem
+(define (sum-up-numbers L)
+    (cond 
+     ((null? L) '())
+     (else ;(display '(it's fine))
+      (+ (car L) (sum-up-numbers (cdr L)))
+           )
+     )
+  )
+
+
+(sum-up-numbers '(1 2 3))
