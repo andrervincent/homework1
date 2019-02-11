0416f9077e5a51d414a8a5ef0499f53db39d4a26
André Vincent <andrevncnt7@gmail.com>
2019-02-10 23:48:34 -0500

    Fixes logic for nest list function

--- fpl.scm
+++ fpl.scm
@@ -13,11 +13,16 @@
 (display (deep-reverse '(1 2 (7 8))))
 
 ;; Sum of list problem - no nested lists
-(define (sum-up-numbers L)
-  (if (null? L) 0		;; null lists return 0
-      (+ (car L) (sum-up-numbers (cdr L))))) ;; 
-(sum-up-numbers '(1 2 3))
+(define (sum-numbers L)
+  (if (null? L) 0
+    (if (not (number? (car L))) (+ 0 (sum-numbers (cdr L))) ;; for non-integer elements
+      (+ (car L) (sum-numbers (cdr L)))
+      )
+    )
+  
+)
 
+(display (sum-numbers '(2 1 B 5 3)))
 ;; Sum of list problem - nested lists
 (define (sum-up-numbers-general L)
  (if
